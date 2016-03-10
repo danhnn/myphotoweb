8ed08ea2ca09b33ebb9a4d7afd3463428088ed63
Kha Nguyen <kha@tinypulse.com>
2016-03-10 19:59:58 +0700

    milestone 5

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310122732) do
+ActiveRecord::Schema.define(version: 20160310125831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "photo_id"
+  end
+
+  add_index "comments", ["photo_id"], name: "index_comments_on_photo_id", using: :btree
 
   create_table "photos", force: :cascade do |t|
     t.string   "username"
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 20160310122732) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "comments", "photos"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-Photo.create(username: 'sarahmgellar', caption: '...', created_at: Time.at(14447197352).utc.to_datetime, likes_count: 10561, url: "http://lorempixel.com/400/200")
-Photo.create(username: 'sarahmgellar', caption: '...', created_at: Time.at(14447197352).utc.to_datetime, likes_count: 10561, url: "http://lorempixel.com/400/200")
-Photo.create(username: 'sarahmgellar', caption: '...', created_at: Time.at(14447197352).utc.to_datetime, likes_count: 10561, url: "http://lorempixel.com/400/200")
-Photo.create(username: 'sarahmgellar', caption: '...', created_at: Time.at(14447197352).utc.to_datetime, likes_count: 10561, url: "http://lorempixel.com/400/200")
+#Photo.create(username: 'sarahmgellar', caption: '...', created_at: Time.at(14447197352).utc.to_datetime, likes_count: 10561, url: "http://lorempixel.com/400/200")
+
+Photo.all.each do |photo|
+  rand(10).times do
+    photo.comments << Comment.new(name: 'NND', body: 'Just a test comment' )
+  end
+end
