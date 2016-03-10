7698ea756edd5486ef77f293215973a7b5d63ebd
Kha Nguyen <kha@tinypulse.com>
2016-03-10 19:49:12 +0700

    milestone 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Photo.create(username: 'sarahmgellar', caption: '...', created_at: Time.at(14447197352).utc.to_datetime, likes_count: 10561, url: "http://lorempixel.com/400/200")
+Photo.create(username: 'sarahmgellar', caption: '...', created_at: Time.at(14447197352).utc.to_datetime, likes_count: 10561, url: "http://lorempixel.com/400/200")
+Photo.create(username: 'sarahmgellar', caption: '...', created_at: Time.at(14447197352).utc.to_datetime, likes_count: 10561, url: "http://lorempixel.com/400/200")
+Photo.create(username: 'sarahmgellar', caption: '...', created_at: Time.at(14447197352).utc.to_datetime, likes_count: 10561, url: "http://lorempixel.com/400/200")
